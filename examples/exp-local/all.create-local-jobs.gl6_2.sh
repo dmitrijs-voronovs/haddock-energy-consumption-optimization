c805089f8352788ee57c098cbbb912c08fbecba9
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2023-12-14 16:09:31 +0100

    Run GL6_2 experiment

---
+++ examples/exp-local/all.create-local-jobs.gl6_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+sh create-local-job.sh daa 4 gl6 11
+sh create-local-job.sh daa 8 gl6 11
+sh create-local-job.sh daa 8 gl6 12
+sh create-local-job.sh daa 8 gl6 13
+sh create-local-job.sh daa 8 gl6 14
+sh create-local-job.sh dpp 4 gl6 11
+sh create-local-job.sh dpp 4 gl6 12
+sh create-local-job.sh dpp 8 gl6 11
+sh create-local-job.sh dpp 8 gl6 12
+sh create-local-job.sh dpp 16 gl6 11
+sh create-local-job.sh dpp 32 gl6 11
+sh create-local-job.sh dpp 8 gl6 1 warmup
