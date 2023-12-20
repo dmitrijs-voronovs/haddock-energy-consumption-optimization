935c1cc3fcd8f5db607517fd00e9be8d7d248193
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2023-12-20 13:41:41 +0100

    Update experiment classes

--- examples/domain/LocalExperimentGL2_3.py
+++ examples/domain/LocalExperimentGL2_3.py
@@ -1,13 +1,14 @@
-
 from typing import List
 
 from examples.domain import LocalExperiment
 from examples.domain.Config import Config
 from examples.domain.LocalConfig import LocalConfig
 
+
 class LocalExperimentGl2_3(LocalExperiment):
     def create_configs(self) -> List[Config]:
-        return [LocalConfig("daa", "gl2", trial, 8) for trial in range(20, 20 + 1)] + [LocalConfig("dpp", "gl2", trial, 8) for trial in range(20, 20 + 1)]
+        return [LocalConfig("daa", "gl2", trial, 8) for trial in range(20, 20 + 1)] + [
+            LocalConfig("dpp", "gl2", trial, 8) for trial in range(20, 20 + 1)]
 
     def create_warmup_config(self) -> Config:
-        return LocalConfig("daa", "gl2", 20, 8)
+        return LocalConfig("dpp", "gl2", 20, 32)
