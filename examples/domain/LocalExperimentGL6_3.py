e46f316814408206ab23a5c257b3a4205fe4f01e
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2023-12-20 13:34:08 +0100

    Update experiment classes

--- examples/domain/LocalExperimentGL6_3.py
+++ examples/domain/LocalExperimentGL6_3.py
@@ -1,18 +1,13 @@
+
 from typing import List
 
-from examples.domain import Config, LocalConfig, LocalExperiment
+from examples.domain import LocalExperiment
+from examples.domain.Config import Config
+from examples.domain.LocalConfig import LocalConfig
 
-
-class LocalExperimentGL6_3(LocalExperiment):
+class LocalExperimentGl6_3(LocalExperiment):
     def create_configs(self) -> List[Config]:
-        return [LocalConfig("daa", "gl6", trial, 4) for trial in range(11, 11 + 1)] + [
-            LocalConfig("daa", "gl6", trial, 8) for trial in range(11, 11 + 4)] + [LocalConfig("daa", "gl6", trial, 16)
-                                                                                   for trial in range(11, 11 + 0)] + [
-            LocalConfig("daa", "gl6", trial, 32) for trial in range(11, 11 + 0)] + [LocalConfig("dpp", "gl6", trial, 4)
-                                                                                    for trial in range(11, 11 + 2)] + [
-            LocalConfig("dpp", "gl6", trial, 8) for trial in range(11, 11 + 2)] + [LocalConfig("dpp", "gl6", trial, 16)
-                                                                                   for trial in range(11, 11 + 1)] + [
-            LocalConfig("dpp", "gl6", trial, 32) for trial in range(11, 11 + 1)]
+        return [LocalConfig("daa", "gl6", trial, 4) for trial in range(20, 20 + 1)] + [LocalConfig("dpp", "gl6", trial, 4) for trial in range(20, 20 + 1)]
 
     def create_warmup_config(self) -> Config:
-        return LocalConfig("dpp", "gl6", 1, 20, True)
+        return LocalConfig("daa", "gl6", 20, 4)
