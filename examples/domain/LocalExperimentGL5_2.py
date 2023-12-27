dd0bdd198d642e4f0b711eface619a98d0e96cf2
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2023-12-27 02:28:52 +0100

    Update experiment results for gl5

--- examples/domain/LocalExperimentGL5_2.py
+++ examples/domain/LocalExperimentGL5_2.py
@@ -8,7 +8,7 @@ from examples.domain.LocalConfig import LocalConfig
 
 class LocalExperimentGL5_2(LocalExperiment):
     def create_configs(self) -> List[Config]:
-        return [LocalConfig("daa", "gl5", trial, 4) for trial in range(10, 10 + 8)] + [LocalConfig("daa", "gl5", trial, 8) for trial in range(10, 10 + 6)] + [LocalConfig("daa", "gl5", trial, 16) for trial in range(10, 10 + 8)] + [LocalConfig("daa", "gl5", trial, 32) for trial in range(10, 10 + 9)] + [LocalConfig("dpp", "gl5", trial, 4) for trial in range(10, 10 + 7)] + [LocalConfig("dpp", "gl5", trial, 8) for trial in range(10, 10 + 7)] + [LocalConfig("dpp", "gl5", trial, 16) for trial in range(10, 10 + 9)] + [LocalConfig("dpp", "gl5", trial, 32) for trial in range(10, 10 + 6)]
+        return [LocalConfig("daa", "gl5", trial, 4) for trial in range(10, 10 + 4)] + [LocalConfig("daa", "gl5", trial, 8) for trial in range(10, 10 + 5)] + [LocalConfig("daa", "gl5", trial, 16) for trial in range(10, 10 + 3)] + [LocalConfig("daa", "gl5", trial, 32) for trial in range(10, 10 + 4)] + [LocalConfig("dpp", "gl5", trial, 4) for trial in range(10, 10 + 3)] + [LocalConfig("dpp", "gl5", trial, 8) for trial in range(10, 10 + 4)] + [LocalConfig("dpp", "gl5", trial, 16) for trial in range(10, 10 + 2)] + [LocalConfig("dpp", "gl5", trial, 32) for trial in range(10, 10 + 4)]
 
     def create_warmup_config(self) -> Config:
         return LocalConfig("dpp", "gl5", 10, 32, True)
