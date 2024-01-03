65e8e72551fecf1d406e6ba6e31cfd47e6a8ec01
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2024-01-03 02:46:38 +0200

    Create Test_2 HPC experiment

---
+++ examples/exp-hpc/create-jobs.test_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+sh create-job.sh dpp 1 18 gl2-gl5-gl6 11
+sh create-job.sh dpp 1 9 gl2-gl5-gl6 11
+sh create-job.sh dpp 5 18 gl2-gl5-gl6 11
+sh create-job.sh dpp 5 9 gl2-gl5-gl6 11
+sh create-job.sh dpp 10 18 gl2-gl5-gl6 11
+sh create-job.sh dpp 10 9 gl2-gl5-gl6 11
