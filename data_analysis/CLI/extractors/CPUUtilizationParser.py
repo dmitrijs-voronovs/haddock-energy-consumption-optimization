60ebcf8d5288fff4965c7a897fd9ec73481be773
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2024-01-28 01:38:07 +0100

    Update interface

--- data_analysis/CLI/extractors/CPUUtilizationParser.py
+++ data_analysis/CLI/extractors/CPUUtilizationParser.py
@@ -19,12 +19,13 @@ class CPUUtilizationParser(IndividualParser):
             return timestamp.date()
 
     @staticmethod
-    def get_timestamp(prev_timestamp, date, time):
+    def get_timestamp(prev_date, date, timestamp_str):
+        time = datetime.strptime(timestamp_str, '%I:%M:%S %p').time()
         timestamp = datetime.combine(date, time)
 
         new_date = date
         # Check if day has changed
-        if prev_timestamp and timestamp < datetime.fromisoformat(prev_timestamp):
+        if prev_date and timestamp < datetime.fromisoformat(prev_date):
             new_date += timedelta(days=1)
             timestamp = datetime.combine(new_date, time)
         return timestamp, new_date
@@ -48,9 +49,8 @@ class CPUUtilizationParser(IndividualParser):
 
                     if match:
                         timestamp_str = match.group(1)
-                        prev_timestamp = cpu_data[-1]['Timestamp'] if cpu_data else None
-                        time = datetime.strptime(timestamp_str, '%I:%M:%S %p').time()
-                        timestamp, date = CPUUtilizationParser.get_timestamp(prev_timestamp, date, time)
+                        prev_date = cpu_data[-1]['Timestamp'] if cpu_data else None
+                        timestamp, date = CPUUtilizationParser.get_timestamp(prev_date, date, timestamp_str)
 
                         cpu_info = match.groups()
                         cpu_data.append(
